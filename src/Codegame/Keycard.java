fbdb0eb406776a1edd33c50c8404ab44486a7f76
Zijian Wang <kramerzj@gmail.com>
2019-01-23 21:05:42 -0500

    very basic classes
    I just add attributes and some getters and setters.

---
+++ src/Codegame/Keycard.java
@@ -0,0 +1,10 @@
+package Codegame;
+
+public class Keycard extends Board {
+	public Keycard(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+	private int width;
+	private int height;
+}
---
+++ src/Codegame/Player.java
@@ -0,0 +1,11 @@
+package Codegame;
+
+public class Player {
+ private int team;//1 for red, 0 for blue. 
+ public int getTeam() {
+	 return team;
+ }
+ public void setTeam(int teamType) {
+	 this.team=teamType;
+ }
+}
