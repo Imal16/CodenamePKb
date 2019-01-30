f2b496a1a4e9c26157d8fe8a0cb9d33e201a34c9
rteasdale <rosy.teasdale@gmail.com>
2019-01-30 18:51:59 -0500

    fxml test

---
+++ src/res/ControllerEx.java
@@ -0,0 +1,14 @@
+package res;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class ControllerEx {
+	@FXML private GridPane grid;
+	
+	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        //Action handler
+    }
+
+}
