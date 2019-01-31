caa6f3cd846bb1492141c48355d3a9212cc6eaf7
rteasdale <rosy.teasdale@gmail.com>
2019-01-30 19:07:48 -0500

    FXML update

---
+++ src/res/Card.java
@@ -0,0 +1,58 @@
+package res;
+
+import javafx.geometry.HPos; 
+import javafx.geometry.VPos;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class Card extends Rectangle {
+	
+	private Text word;
+	private Rectangle card;
+	private Color color;
+	private int SIZE_CARDS = 100;
+	
+	/**
+	 * Default
+	 */
+	public Card() {
+		this.card = new Rectangle(SIZE_CARDS, SIZE_CARDS, Color.LIGHTGRAY);
+		this.word = new Text("Word");
+		GridPane.setHalignment(this.card, HPos.CENTER);
+		GridPane.setValignment(this.card, VPos.CENTER);
+	}
+	
+	public Card(Text word, Color color) {
+		this.card = new Rectangle(SIZE_CARDS, SIZE_CARDS, color);
+		this.word = word;
+		GridPane.setHalignment(this.card, HPos.CENTER);
+		GridPane.setValignment(this.card, VPos.CENTER);
+	}
+	
+	public void setColor(Color color) {
+		this.card.setFill(color);
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	
+	public void setWord(Text word) {
+		this.word = word;
+	}
+	
+	public Text getWord() {
+		return this.word;
+	}
+	
+	public void setCard(Rectangle card) {
+		this.card = card;
+	}
+	
+	public Rectangle getCard() {
+		return this.card;
+	}
+
+}
--- src/res/ControllerEx.java
+++ src/res/ControllerEx.java
@@ -10,5 +10,20 @@ public class ControllerEx {
 	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
         //Action handler
     }
+	
+    public void createBoard() {
+    	
+        for(int i=0;i<5;i++){
+        	
+           for(int j=0;j<5;j++){
+        	   
+               grid.add(new GridPane(), i, j);
+               
+           }
+           
+        }
+        
+
+    }
 
 }
